185dbb69ddcadf91abd1f7c9d0fc118280e52edd
Paz Rotllan Garcia <pazrotgar@gmail.com>
2025-01-30 21:01:46 +0100

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,26 +1,22 @@
 // api.js
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
-exports.handler = async (event, context) => {
-  if (event.httpMethod === 'GET') {
-    try {
-      // Process the GET request as needed
-      const filePath = path.join(__dirname, 'db.json'); // Adjusted path
-      const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+exports.handler = async () => {
+  try {
+    const filePath = path.join(__dirname, 'db.json'); // Ensure db.json is inside functions
+    const data = await fs.readFile(filePath, 'utf-8');
 
-      // Return the data as the response
-      return {
-        statusCode: 200,
-        body: JSON.stringify(data),
-        headers: { "Content-Type": "application/json" },
-      };
-    } catch (error) {
-      // Return an error response if there was an issue processing the request
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: 'Failed to process GET request' }),
-      };
-    }
+    return {
+      statusCode: 200,
+      body: data,
+      headers: { "Content-Type": "application/json" },
+    };
+  } catch (error) {
+    console.error("Error reading db.json:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to process GET request" }),
+    };
   }
 };
