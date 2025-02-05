4ac1ae26a8471f14ecc4978213aa6afa430a334b
Paz Rotllan Garcia <pazrotgar@gmail.com>
2025-02-05 22:13:26 +0100

    Update peludos.js

--- netlify/functions/peludos.js
+++ netlify/functions/peludos.js
@@ -17,6 +17,7 @@ exports.handler = async (event, context) => {
 
     // Fetch the db.json data
     let result = await axios.get("https://buscador-lasanimal.netlify.app/perro.json", {});
+    console.log(result)
     /*
     if (response.status !== 200) {
       throw new Error(`HTTP error! status: ${response.status}`);
